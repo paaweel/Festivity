e156e547f576cb8230a1994a79ca393e5a48556a
pawel <pol4_1997@o2.pl>
2018-11-25 23:57:24 +0100

    Festival class

--- Festivity/Festival.cs
+++ Festivity/Festival.cs
@@ -8,11 +8,21 @@ namespace Festivity
 {
     class Festival
     {
+        public Festival()
+        {
+            Locations = new int[3, 3] {
+                {0, 10, 20},
+                {20, 0, 10},
+                {10, 20, 0}
+            };
+        }
         public List<Shift> Shifts;
         public List<Person> People;
-        public TimeSpan GetTravelTime()
+        public int[,] Locations;
+        public TimeSpan GetTravelTime(int Location1, int Location2)
         {
-            return new TimeSpan();
+            TimeSpan TravelTime = new TimeSpan(0, Locations[Location1, Location2], 0);
+            return TravelTime;
         }
     }
 }
