6792c7dcd9e75737151d7407cf5b5871126b42f4
pawel <pol4_1997@o2.pl>
2018-11-25 23:57:56 +0100

    Solution class

--- Festivity/Solution.cs
+++ Festivity/Solution.cs
@@ -8,7 +8,7 @@ namespace Festivity
 {
     class Solution
     {
-        public Dictionary<Person, Shift> Assignment;
+        public Dictionary<Person, List<Shift>> Assignment;
         public double? Fitness;
         public bool Validate()
         {
@@ -18,7 +18,27 @@ namespace Festivity
         {
             if (Fitness == null)
             {
-                // calculate fitness
+                // calculate Fitness
+                // 1 - calculate avarege load
+                int nOfPeople = Assignment.Count;
+                int totalLoad = 0;
+                foreach (KeyValuePair<Person, List<Shift>> entry in Assignment)
+                {
+                    totalLoad += entry.Value.Sum(item => item.When.GetDuration().Minutes);                 
+                }
+                double averageLoad = totalLoad / nOfPeople;
+
+
+                
+                Fitness = 0;
+                foreach(KeyValuePair<Person, List<Shift>> entry in Assignment)
+                {
+                    int load = entry.Value.Sum(item => item.When.GetDuration().Minutes);
+                    foreach (var shift in entry.Value)
+                    {
+                        Fitness += Math.Sqrt(Math.Pow(load - averageLoad, 2.00));
+                    }
+                }
             }
 
             return (double)Fitness;
