69b2af1761f2290e3d37c715d2a5305e2d45df8e
pawel <pol4_1997@o2.pl>
2018-12-09 10:39:11 +0100

    DataLoader class

---
+++ Festivity/IDataLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivity
+{
+    interface IDataLoader
+    {
+        Festival LoadData();
+    }
+
+    class DataLoaderConsole : IDataLoader
+    {
+        public Festival LoadData()
+        {
+            return new Festival();
+        }
+    }
+
+    class DataLoaderJson : IDataLoader
+    {
+        public Festival LoadData()
+        {
+            return new Festival();
+        }
+    }
+
+    class DataLoaderApp : IDataLoader
+    {
+        public Festival LoadData()
+        {
+            return new Festival();
+        }
+    }
+}
