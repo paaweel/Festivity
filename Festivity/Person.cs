50413f74a637404c8e247b9fbe44f95ba127d428
pawel <pol4_1997@o2.pl>
2018-12-09 10:43:11 +0100

    Minor changes to person

--- Festivity/Person.cs
+++ Festivity/Person.cs
@@ -9,15 +9,17 @@ namespace Festivity
     class Person
     {
         private static int _id = 0;
-        public int id;
+        public int Id { get; private set; }
+        public List<TimeTable> Availability { get; private set; }
+        public List<TimeTable> Preferences { get; private set; }
 
-        public Person()
+        public Person(List<TimeTable> availability, List<TimeTable> preferences = null)
         {
-            id = _id;
-        }
-
-        public List<TimeTable> Availability { get; set; }
-        public List<TimeTable> Preferences { get; set; }
-        
+            Id = _id;
+            _id++;
+            if (availability != null) Availability = availability;
+            if (preferences != null) Preferences = preferences
+    
+        }      
     }
 }
