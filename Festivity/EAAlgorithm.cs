7d91d703895f32e3118d6136f87b582d1ebf62f3
pawel <pol4_1997@o2.pl>
2018-12-09 10:37:55 +0100

    minor changes and evaluate fnc

--- Festivity/EAAlgorithm.cs
+++ Festivity/EAAlgorithm.cs
@@ -13,12 +13,13 @@ namespace Festivity
         public List<Solution> Population; //extract to another class?
         public Solution BestSolution;
         private static Random RandomGen = new Random();
+        public IDataLoader dataLoader = new DataLoaderJson();
 
         public EAAlgorithm(Festival festival, int populationSize = 10)
         {
             BestSolution = new Solution(festival);
-            this.PopulationSize = populationSize;
-            this.Festival = festival;
+            PopulationSize = populationSize;
+            Festival = festival;
             Population = new List<Solution>();
             
             for (int i = 0; i < PopulationSize; ++i)
@@ -30,11 +31,22 @@ namespace Festivity
 
         public void Evolve()
         {
-
+            
         }
+
         public void Mutate()
         {
 
+        }
+
+        public void Evaluate()
+        {
+            Population.Sort((x, y) => x.Evaluate().CompareTo(y.Evaluate())); //sort by fitness
+            if (BestSolution.Evaluate() < Population[0].Evaluate())
+            {
+                BestSolution = Population[0];
+                //event - bestSolution changed
+            }
         }
 
         public void CreateInitial()
