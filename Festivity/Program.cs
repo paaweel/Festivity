fccaf60e2500feb6f280069655a567f5c869332a
pawel <pol4_1997@o2.pl>
2018-12-11 10:13:56 +0100

    example of working data loader and creator

--- Festivity/Program.cs
+++ Festivity/Program.cs
@@ -10,31 +10,70 @@ namespace Festivity
     {
         static void Main(string[] args)
         {
+            DataCreator d = new DataCreator();
+            d.CreateAndSaveDataToFile("test", 3, 3);
+            IDataLoader l = new DataLoaderJson();
+            Festival f = l.LoadData("test");
 
-            TimeTable t1 = new TimeTable();
+            Console.WriteLine(f.People[].Id);
+
+            Console.ReadKey();
+            //TimeTable t1 = new TimeTable();
 
 
-  //          Festival festival = new Festival();
-    //        EAAlgorithm alg = new EAAlgorithm(festival,5);
-      //      int i = 0;
-        //    foreach (var sol in alg.Population)
-          //  {
+            //          Festival festival = new Festival();
+            //        EAAlgorithm alg = new EAAlgorithm(festival,5);
+            //      int i = 0;
+            //    foreach (var sol in alg.Population)
+            //  {
             //    i++;
-              //  Console.WriteLine($"\n{i}th solution");
-                //foreach (KeyValuePair<Person, List<Shift>> entry in sol.Assignment)
-                //{
-                  //  Console.WriteLine($"Person: {entry.Key.id} has following shifts assigned:");
-                    //foreach (var shift in entry.Value)
-                    //{
-                      //  Console.Write(shift.id);
-                        //Console.Write(" ");
-                    //}
-                    //Console.WriteLine("");
-                //}
+            //  Console.WriteLine($"\n{i}th solution");
+            //foreach (KeyValuePair<Person, List<Shift>> entry in sol.Assignment)
+            //{
+            //  Console.WriteLine($"Person: {entry.Key.id} has following shifts assigned:");
+            //foreach (var shift in entry.Value)
+            //{
+            //  Console.Write(shift.id);
+            //Console.Write(" ");
+            //}
+            //Console.WriteLine("");
+            //}
             //}
             //Console.ReadKey();
+            /*
+            Locations = new int[3, 3] {
+                {0, 10, 20},
+                {20, 0, 10},
+                {10, 20, 0}
+            };
 
+            People = new List<Person>();
 
+            People.Add(new Person());
+            People.Add(new Person());
+            People.Add(new Person());
+
+            Shifts = new List<Shift>();
+
+            Shifts.Add(new Shift(new DateTime(2018, 10, 10, 10, 10, 10, 10),    //start
+                                 new DateTime(2018, 10, 10, 11, 10, 10, 10),    //end
+                                 3,                                             //pplNeeded
+                                 1,                                             //loc
+                                 0));
+
+            Shifts.Add(new Shift(new DateTime(2018, 10, 11, 10, 10, 10, 10),
+                                 new DateTime(2018, 10, 11, 13, 10, 10, 10),
+                                 2,
+                                 0,
+                                 1));
+
+            Shifts.Add(new Shift(new DateTime(2018, 10, 10, 11, 10, 10, 10),
+                                 new DateTime(2018, 10, 10, 12, 10, 10, 10),
+                                 1,
+                                 2,
+                                 2));
+        */
         }
+
     }
 }
